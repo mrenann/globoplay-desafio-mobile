f9b59f854a5962b66da62085b585dc5c857026fe
mrenann <marcos.renann.br@gmail.com>
2024-12-17 09:45:53 -0300

    feat: Initialize Koin dependency injection and create application class

--- app/src/main/java/com/mrenann/globoplay/MainActivity.kt
+++ app/src/main/java/com/mrenann/globoplay/MainActivity.kt
@@ -4,19 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.mrenann.globoplay.di.appModule
 import com.mrenann.globoplay.ui.theme.GloboplayTheme
+import org.koin.core.context.startKoin
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        startKoin {
+            modules(appModule)
+        }
+
         setContent {
             GloboplayTheme {
 
