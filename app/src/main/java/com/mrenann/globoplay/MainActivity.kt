8f3b58834ab2f010700f5c0f9035194466f03a5e
mrenann <marcos.renann.br@gmail.com>
2024-12-18 13:57:47 -0300

    feat: Implement Search Screen

--- app/src/main/java/com/mrenann/globoplay/MainActivity.kt
+++ app/src/main/java/com/mrenann/globoplay/MainActivity.kt
@@ -14,6 +14,7 @@ import com.mrenann.globoplay.initialScreen.presentation.InitialScreen
 import com.mrenann.globoplay.mediaDetailsScreen.di.MovieDetailsModule
 import com.mrenann.globoplay.mediaDetailsScreen.di.TvDetailsModule
 import com.mrenann.globoplay.myListScreen.di.favoriteModule
+import com.mrenann.globoplay.searchScreen.di.searchModule
 import com.mrenann.globoplay.ui.theme.GloboplayTheme
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
@@ -34,7 +35,8 @@ class MainActivity : ComponentActivity() {
                     MovieDetailsModule,
                     TvDetailsModule,
                     roomModule,
-                    favoriteModule
+                    favoriteModule,
+                    searchModule
                 )
             } catch (e: Exception) {
                 e.printStackTrace()
