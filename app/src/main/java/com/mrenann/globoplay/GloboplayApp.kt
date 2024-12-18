edfbeeba8aafb9c175303034ddb07c90f0abbbef
mrenann <marcos.renann.br@gmail.com>
2024-12-18 14:17:55 -0300

    chore: Initialize Koin in GloboplayApp

--- app/src/main/java/com/mrenann/globoplay/GloboplayApp.kt
+++ app/src/main/java/com/mrenann/globoplay/GloboplayApp.kt
@@ -1,5 +1,36 @@
 package com.mrenann.globoplay
 
 import android.app.Application
+import com.mrenann.globoplay.core.di.networkModule
+import com.mrenann.globoplay.core.di.roomModule
+import com.mrenann.globoplay.homeScreen.di.MovieDiscoverModule
+import com.mrenann.globoplay.homeScreen.di.TvDiscoverModule
+import com.mrenann.globoplay.mediaDetailsScreen.di.MovieDetailsModule
+import com.mrenann.globoplay.mediaDetailsScreen.di.TvDetailsModule
+import com.mrenann.globoplay.myListScreen.di.favoriteModule
+import com.mrenann.globoplay.searchScreen.di.searchModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
-class GloboplayApp : Application()
+class GloboplayApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            try {
+                androidContext(applicationContext)
+                modules(
+                    networkModule,
+                    MovieDiscoverModule,
+                    TvDiscoverModule,
+                    MovieDetailsModule,
+                    TvDetailsModule,
+                    roomModule,
+                    favoriteModule,
+                    searchModule
+                )
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
--- app/src/main/java/com/mrenann/globoplay/MainActivity.kt
+++ app/src/main/java/com/mrenann/globoplay/MainActivity.kt
@@ -6,43 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import cafe.adriel.voyager.navigator.Navigator
-import com.mrenann.globoplay.core.di.networkModule
-import com.mrenann.globoplay.core.di.roomModule
-import com.mrenann.globoplay.homeScreen.di.MovieDiscoverModule
-import com.mrenann.globoplay.homeScreen.di.TvDiscoverModule
 import com.mrenann.globoplay.initialScreen.presentation.InitialScreen
-import com.mrenann.globoplay.mediaDetailsScreen.di.MovieDetailsModule
-import com.mrenann.globoplay.mediaDetailsScreen.di.TvDetailsModule
-import com.mrenann.globoplay.myListScreen.di.favoriteModule
-import com.mrenann.globoplay.searchScreen.di.searchModule
 import com.mrenann.globoplay.ui.theme.GloboplayTheme
-import org.koin.android.ext.koin.androidContext
-import org.koin.core.context.startKoin
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         enableEdgeToEdge()
-
-        startKoin {
-            try {
-                androidContext(applicationContext)
-                modules(
-                    networkModule,
-                    MovieDiscoverModule,
-                    TvDiscoverModule,
-                    MovieDetailsModule,
-                    TvDetailsModule,
-                    roomModule,
-                    favoriteModule,
-                    searchModule
-                )
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
-
 
         setContent {
             GloboplayTheme {
